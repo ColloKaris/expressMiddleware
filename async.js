ba2675934e63918afad00a6c6b5f9627caff88c5
Collins Kariuki <collinsmunkary@gmail.com>
2023-06-25 10:21:55 +0300

    Creating promises- Async refresher

---
+++ async.js
@@ -0,0 +1,23 @@
+// Making our own promises
+
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if(rand < 0.7) {
+                resolve('YOUR FAKE DATA HERE');
+            }
+            reject('REQUEST ERROR');
+        }, 1000)
+    })
+}
+
+fakeRequest('/dogs/1')
+.then((data) => {
+    console.log("DONE WITH REQUEST")
+    console.log("Data is:", data)
+})
+.catch((err) => {
+    console.log("OH NO!!", err)
+})
