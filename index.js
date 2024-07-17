22c7c22d719b4d8108663a6e77ff674e863a5213
Collins Kariuki <collinsmunkary@gmail.com>
2024-07-17 10:28:41 +0300

    Custom error class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // require express
 const express = require('express');
+const AppError = require('./AppError')
 
 //execute express
 const app = express();
@@ -25,7 +26,7 @@ const verifyPassword = (req,res,next) => {
         next();
     }
     //res.send('SORRY YOU NEED A PASSWORD')
-    throw new Error("Password required!!!")
+    throw new AppError('Password required!', 401);
 }
 
 app.get('/secret', verifyPassword ,(req,res) => {
@@ -50,22 +51,29 @@ app.get('/dogs', (req,res) => {
     res.send('WOOF WOOF!!')
 })
 
+app.get('/admin', (req,res) => {
+    throw new AppError('You are not an Admin', 403)
+})
+
 // Add a 404 page
 app.use((req,res) =>{
     res.status(404).send("404 - NOT FOUND")
 })
 
 // Error Handling middleware
+// app.use((err, req, res, next) => {
+//     console.log("****************************************")
+//     console.log("******************ERROR*****************")
+//     console.log("****************************************")
+//     res.status(500).send("OH BOY, WE GOT AN ERROR!!!")
+//     console.log(err);
+//     next(err);
+// })
+
 app.use((err, req, res, next) => {
-    console.log("****************************************")
-    console.log("******************ERROR*****************")
-    console.log("****************************************")
-    //res.status(500).send("OH BOY, WE GOT AN ERROR!!!")
-    console.log(err)
-    next(err);
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
 })
-
-
 
 //listen at port 3000
 app.listen(3000, () => {
